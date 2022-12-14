62bed3fa1d2ac4f18edc5caaf8af54ce788992e7
Mohan Ram T.R <mohanraam2000@gmail.com>
2022-12-14 12:31:54 +0530

    14-12-22

---
+++ DatabaseFirstApproach/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseFirstApproach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ShowAPIDataReact/DataTransferBook.cs
@@ -0,0 +1,13 @@
+﻿namespace ShowAPIDataReact
+{
+    public class DataTransferBook
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+
+    }
+}
